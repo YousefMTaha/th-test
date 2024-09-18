72b46890a54c5763eb24e316819fe38b8f1aa394
Yousef Taha <tyousef262@gmail.com>
2024-09-18 22:32:48 +0300

    fixed bug

--- src/fileupload/fileupload.js
+++ src/fileupload/fileupload.js
@@ -2,26 +2,22 @@ import multer, { diskStorage } from "multer";
 import { nanoid } from "nanoid";
 import { AppError } from "../utils/appError.js";
 
-export const fileupload = (foldername)=>{
-    const storage = diskStorage({
-        destination: `uploads/${foldername}`,
-        filename:(req,file,cb)=>{
-            cb(null, nanoid() + "_" + file.originalname)
-        }
-    })
+export const fileupload = (foldername) => {
+  const storage = diskStorage();
 
-    function filefilter (req,file,cb){
-        if(file.mimetype.startWith('image')){
-            cb(null,true)
-        }else{
-            cb(new AppError('images only' ,401) , false)
-        }
+  function filefilter(req, file, cb) {
+    if (file.mimetype.startWith("image")) {
+      cb(null, true);
+    } else {
+      cb(new AppError("images only", 401), false);
     }
+  }
 
-    const uplaod =multer({storage,filefilter})
-    return uplaod
+  const uplaod = multer({ storage, filefilter });
+  return uplaod;
+};
 
-}
-
-export const uplaodsinglefile = (fieldname,foldername) => fileupload(foldername).single(fieldname)
-export const uplaodsmixfiles = (arrayoffileds,foldername) => fileupload(foldername).fields(arrayoffileds)  
+export const uplaodsinglefile = (fieldname, foldername) =>
+  fileupload(foldername).single(fieldname);
+export const uplaodsmixfiles = (arrayoffileds, foldername) =>
+  fileupload(foldername).fields(arrayoffileds);
